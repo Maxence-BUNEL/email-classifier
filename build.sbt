e3e93d6e254e63e227b9a9d9a1559a836035ffc7
Maxence BUNEL <mbunel@linagora.com>
2017-01-09 13:40:32 +0100

    Comment added for testing purpose

--- build.sbt
+++ build.sbt
@@ -7,5 +7,6 @@ organization := "com.linagora"
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-"org.apache.spark" %% "spark-core" % "2.0.0"
+"org.apache.spark" %% "spark-core" % "2.0.0",
+"org.apache.spark" %% "spark-sql" % "2.0.0"
 )
--- src/main/scala-2.11/Test.scala
+++ src/main/scala-2.11/Test.scala
@@ -1,10 +1,42 @@
 import org.apache.spark._
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.log4j._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+
 
 object Test {
   def main(args: Array[String]): Unit = {
-    val sc = new SparkContext("local[*]", "test")
-    val data = sc.parallelize(Seq("a" , "b" , "c"))
-    val withIndex = data.zipWithIndex();
-    withIndex.foreach(println(_))
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    // Use new SparkSession interface in Spark 2.0
+    val spark = SparkSession
+      .builder
+      .appName("Json Parser")
+      .master("local[*]")
+      .getOrCreate()
+
+    val mails = spark.read.json("mails/-mail_zied.json")
+      .select("_source.*")
+      .select("mailboxId", "from", "to", "cc", "bcc")
+
+    val mailboxes = spark.read.csv("mails/usersmailfoldersid.csv").toDF("mailboxId", "mailboxName")
+
+    val mailsWithMailboxNames = mails.join(mailboxes, Seq("mailboxId"))
+//test comment
+    mails.printSchema()
+    mailboxes.printSchema()
+    mailsWithMailboxNames.printSchema()
+
+    mailsWithMailboxNames.limit(1).foreach(printEntry(_))
+  }
+
+  def printEntry(row: Row) : Unit = {
+    val users = row.getAs[Seq[String]]("from")
+    val mailId = ""
+    val mailbox = row.getAs[String]("mailboxName")
+    println(s"mail with users $users for mail $mailId in $mailbox")
   }
 }
